f45e4c70374c36cc899f7fc508387b7ccb388a35
devsunset <devsunset@gmail.com>
2023-02-17 13:59:19 +0900

    dev

--- vctalarm.py
+++ vctalarm.py
@@ -9,15 +9,7 @@
 # import
 from vctinfo import vctinfo
 
-vctalarm = vctinfo.VctInfo()
-
 #################################################
 # main
 if __name__ == '__main__':
-    # moinitor markets info
-    # vctalarm.monitorMarkets(loop=False, looptime=5, sort='signed_change_rate', targetMarket=['KRW'])
-
-    # get candles chart data &  save to db
-    # vctalarm.loadMarketsCandlesMwdData()
-
-    vctalarm.vctAlarm(targetMarket=['KRW'])
+    vctinfo.VctInfo().vctAlarm(targetMarket=['KRW'])
